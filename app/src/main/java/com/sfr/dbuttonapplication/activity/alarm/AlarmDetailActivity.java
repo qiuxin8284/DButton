1b9db5faf291e0ad015664363db33e0df9646459
qiuxin8284 <lili8284>
2019-04-16 12:08:40 +0800

    update

--- app/src/main/java/com/sfr/dbuttonapplication/activity/alarm/AlarmDetailActivity.java
+++ app/src/main/java/com/sfr/dbuttonapplication/activity/alarm/AlarmDetailActivity.java
@@ -147,18 +147,17 @@ public class AlarmDetailActivity extends AppCompatActivity implements View.OnCli
                                     Date date = new Date(Long.valueOf(data[2]));
                                     String pointTime = simpleDateFormat.format(date);
                                     txt.setText(pointTime);
-
-                                    Bitmap viewBitmap = PictureUtil.getViewBitmap(view);
-                                    BitmapDescriptor bitmap = BitmapDescriptorFactory.fromBitmap(viewBitmap);
-                                    OverlayOptions option = new MarkerOptions()
-                                            .position(ll)
-                                            .icon(bitmap);
-                                    mBaiduMap.addOverlay(option);
                                 }
                             } catch (Exception e) {
                                 txt.setText(data[2]);
                                 e.printStackTrace();
                             }
+                            Bitmap viewBitmap = PictureUtil.getViewBitmap(view);
+                            BitmapDescriptor bitmap = BitmapDescriptorFactory.fromBitmap(viewBitmap);
+                            OverlayOptions option = new MarkerOptions()
+                                    .position(ll)
+                                    .icon(bitmap);
+                            mBaiduMap.addOverlay(option);
                             points.add(ll);
 //                            Bitmap viewBitmap = PictureUtil.getViewBitmap(view);
 //                            //构建Marker图标
