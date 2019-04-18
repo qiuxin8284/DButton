92338c05b0538c1ecef2b278d97f3c05716baddb
qiuxin8284 <lili8284>
2019-04-18 22:48:24 +0800

    update

--- app/src/main/java/com/sfr/dbuttonapplication/activity/login/LoadingActivity.java
+++ app/src/main/java/com/sfr/dbuttonapplication/activity/login/LoadingActivity.java
@@ -10,8 +10,10 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.view.View;
 import android.view.Window;
 
 import com.sfr.dbuttonapplication.DButtonApplication;
@@ -64,9 +66,19 @@ public class LoadingActivity extends AppCompatActivity implements PermissionInte
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        APPUtils.setTranslucentGone(this);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //APPUtils.setTranslucentGone(this);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_loading);
+
+        //得到当前界面的装饰视图
+        View decorView = getWindow().getDecorView();
+//        SYSTEM_UI_FLAG_FULLSCREEN表示全屏的意思，也就是会将状态栏隐藏
+        //设置系统UI元素的可见性
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        //隐藏标题栏
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
         //初始化并发起权限申请
         mPermissionHelper = new PermissionHelper(this, this);
         mPermissionHelper.requestPermissions();
